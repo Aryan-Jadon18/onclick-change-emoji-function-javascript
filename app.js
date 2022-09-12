4b8741c46c3d43a77efeaeedb1bf20309de1a5ea
Aryan S. Jadon <95238912+Aryan-Jadon18@users.noreply.github.com>
2022-09-12 23:40:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const closedface= document.querySelector('.closed');
+const openface= document.querySelector('.open');
+closedface.addEventListener('click',()=>{
+    if(openface.classList.contains('open')){
+        openface.classList.add('active');
+        closedface.classList.remove('active');
+    }
+});
+openface.addEventListener('click',()=>{
+    if(closedface.classList.contains('closed')){
+        closedface.classList.add('active');
+        openface.classList.remove('active');
+    }
+});
